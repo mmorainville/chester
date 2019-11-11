717a4ec6b2e4487f9333120197663d0b3cd6973a
Mathieu Morainville <mathieumorainville@hotmail.com>
2019-11-11 21:22:30 +0100

    fix: disable PurgeCSS in production for now and use system variables

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ export default {
   ** Nuxt.js dev-modules
   */
   buildModules: [
-    '@nuxtjs/dotenv'
+    ['@nuxtjs/dotenv', { systemvars: true }]
   ],
   /*
   ** Nuxt.js modules
@@ -48,7 +48,7 @@ export default {
     '@nuxtjs/axios',
     '@nuxtjs/pwa',
     // Doc: https://github.com/Developmint/nuxt-purgecss
-    'nuxt-purgecss',
+    // 'nuxt-purgecss',
     // Doc: https://auth.nuxtjs.org/
     '@nuxtjs/auth'
   ],
