e7e69271f554814cee39a0fbb9b8063c74c85622
Mathieu Morainville <mathieumorainville@hotmail.com>
2019-11-21 23:53:25 +0100

    feat(mobile): improve the navbar behavior on mobile

--- components/AppNavbar.vue
+++ components/AppNavbar.vue
@@ -2,7 +2,7 @@
   nav.navbar
     .container
       .navbar-brand
-        nuxt-link.navbar-item(:to="{name: 'index'}") Chester
+        nuxt-link.navbar-item.has-text-weight-bold.is-size-5(:to="{name: 'index'}") Chester
         a.navbar-burger(role="button", aria-label="menu", aria-expanded="false" @click="isNavbarOpen = !isNavbarOpen" :class="{ 'is-active': isNavbarOpen }")
           span(aria-hidden="true")
           span(aria-hidden="true")
@@ -20,7 +20,7 @@
             | Hello, {{ currentUser.username }}
           .navbar-item
             .buttons
-              button.button(@click="getLinksBookmark" v-if="isClient && $nuxt.$route.name === 'links'") Bookmark
+              button.button(@click="getLinksBookmark" v-if="isClient && $route.name === 'links'") Bookmark
               button.button(@click="$root.$emit('app-navbar:on-export')" v-if="showExportButton") Export
 
               nuxt-link.button.is-light(:to="{name: 'login'}" v-if="!$auth.loggedIn") Log in
@@ -29,83 +29,84 @@
 </template>
 
 <script>
-  export default {
-    name: 'AppNavbar',
+export default {
+  name: 'AppNavbar',
 
-    async created () {
-      try {
-        // await this.$axios.$head('_health')
-        this.currentUser = await this.$axios.$get('users/me')
-      } catch (e) {
-        console.log(e)
-      }
+  async created () {
+    try {
+      // await this.$axios.$head('_health')
+      this.currentUser = await this.$axios.$get('users/me')
+    } catch (e) {
+      console.log(e)
+    }
+  },
+
+  data () {
+    return {
+      isNavbarOpen: false,
+
+      currentUser: null,
+
+      items: [
+        {
+          title: 'About',
+          to: { name: 'about' }
+        },
+        {
+          title: 'Links',
+          to: { name: 'links' }
+        },
+        {
+          title: 'Movies',
+          to: { name: 'movies' }
+        }
+      ]
+    }
+  },
+
+  watch: {
+    $route () {
+      this.isNavbarOpen = false
+    }
+  },
+
+  computed: {
+    showExportButton () {
+      const routesWithExport = ['links', 'movies']
+      return routesWithExport.includes(this.$route.name)
     },
 
-    data () {
-      return {
-        isNavbarOpen: false,
+    isClient () {
+      return process.client
+    }
+  },
 
-        currentUser: null,
+  methods: {
+    getLinksBookmark () {
+      const origin = window.location.origin
+      const bookmark = `javascript:(function(){var%20url%20=%20location.href;%20%20%20%20%20%20var%20title%20=%20document.title%20||%20url;%20%20%20%20%20%20window.open('${origin}/links/create?url='%20+%20encodeURIComponent(url)+'&title='%20+%20encodeURIComponent(title),'_blank','height=600,width=800');})();`
+      console.log(bookmark)
 
-        items: [
-          {
-            title: 'Home',
-            icon: 'home',
-            to: {name: 'index'}
-          },
-          {
-            title: 'About',
-            to: {name: 'about'}
-          },
-          {
-            title: 'Links',
-            to: {name: 'links'}
-          },
-          {
-            title: 'Movies',
-            to: {name: 'movies'}
+      navigator.permissions.query({ name: 'clipboard-write' }).then(async result => {
+        if (result.state === 'granted' || result.state === 'prompt') {
+          try {
+            await navigator.clipboard.writeText(bookmark)
+
+            this.$buefy.toast.open({
+              message: 'Bookmark copied!',
+              type: 'is-success'
+            })
+          } catch (error) {
+            this.$buefy.toast.open({
+              message: 'An error occurred.',
+              type: 'is-danger'
+            })
           }
-        ]
-      }
-    },
-
-    computed: {
-      showExportButton () {
-        const routesWithExport = ['links', 'movies']
-        return routesWithExport.includes(this.$nuxt.$route.name)
-      },
-
-      isClient () {
-        return process.client
-      }
-    },
-
-    methods: {
-      getLinksBookmark () {
-        const origin = window.location.origin
-        const bookmark = `javascript:(function(){var%20url%20=%20location.href;%20%20%20%20%20%20var%20title%20=%20document.title%20||%20url;%20%20%20%20%20%20window.open('${origin}/links/create?url='%20+%20encodeURIComponent(url)+'&title='%20+%20encodeURIComponent(title),'_blank','height=600,width=800');})();`
-        console.log(bookmark)
-
-        navigator.permissions.query({ name: 'clipboard-write' }).then(async result => {
-          if (result.state === 'granted' || result.state === 'prompt') {
-            try {
-              await navigator.clipboard.writeText(bookmark)
-
-              this.$buefy.toast.open({
-                message: 'Bookmark copied!',
-                type: 'is-success'
-              })
-            } catch (error) {
-              this.$buefy.toast.open({
-                message: 'An error occurred.',
-                type: 'is-danger'
-              })
-            }
-          }
-        })
-      }
+        }
+      })
     }
   }
+}
 </script>
 
 <style scoped>
