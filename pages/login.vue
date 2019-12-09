4160775b6be56a0afc0d0c8d65703d50106f3c07
Mathieu Morainville <mathieumorainville@hotmail.com>
2019-12-09 16:58:24 +0100

    WIP: feat(signup): add a signup button

--- pages/login.vue
+++ pages/login.vue
@@ -11,47 +11,72 @@
               .control
                 input.input(type="password" placeholder="Password" v-model="password" required)
             .field
-              button.button.is-primary.is-fullwidth(type="submit" :class="{ 'is-loading': isFetching }") Sign in!
+              .buttons
+                button.button.is-primary(type="submit" :class="{ 'is-loading': isFetching }") Sign in!
+                button.button.is-text(type="button" :class="{ 'is-loading': isFetching }" @click="signup") Sign up!
 
             // a(href="#") or sign in with Facebook
 </template>
 
 <script>
-  export default {
-    name: 'Login',
+export default {
+  name: 'Login',
 
-    data () {
-      return {
-        username: '',
-        password: '',
+  data() {
+    return {
+      username: '',
+      password: '',
 
-        isFetching: false
-      }
+      isFetching: false
+    }
+  },
+
+  methods: {
+    login() {
+      this.isFetching = true
+
+      this.$auth.loginWith('local', {
+        data: {
+          identifier: this.username,
+          password: this.password
+        }
+      })
+        .then(
+          () => this.$buefy.toast.open('Logged In!'),
+          () => this.$buefy.toast.open({
+            message: `An error occurred.`,
+            type: 'is-danger'
+          })
+        )
+        .finally(
+          () => this.isFetching = false
+        )
     },
 
-    methods: {
-      login () {
-        this.isFetching = true
+    async signup() {
+      const username = this.username
+      const password = this.password
 
-        this.$auth.loginWith('local', {
-          data: {
-            identifier: this.username,
-            password: this.password
-          }
+      this.isFetching = true
+
+      try {
+        await this.$axios.$post('auth/local/register', {
+          username,
+          password
         })
-          .then(
-            () => this.$buefy.toast.open('Logged In!'),
-            () => this.$buefy.toast.open({
-              message: `An error occurred.`,
-              type: 'is-danger'
-            })
-          )
-          .finally(
-            () => this.isFetching = false
-          )
+      } catch (error) {
+        console.log(error)
+
+        this.$buefy.toast.open({
+          message: `An error occurred.`,
+          type: 'is-danger'
+        })
       }
+
+      this.isFetching = false
     }
   }
+}
 </script>
 
 <style scoped>
